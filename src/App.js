91087c93e9a8432dd1384dd74e12b98fdb6e7bf0
Priyanshu <99090719+MePriyanshuKumar@users.noreply.github.com>
2022-07-06 17:47:35 +0530

    started after exam

--- src/App.js
+++ src/App.js
@@ -4,16 +4,27 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
+import Alert from './components/Alert';
 
 function App() {
   const [mode, setMode] = useState('light'); // Whether the dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+  }
   const toggleMode = () => {
     if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = 'grey';
+      showAlert("Dark mode is enabled", "success");
     } else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
+      showAlert("Light mode is enabled", "success");
+
 
     }
   }
@@ -21,6 +32,7 @@ function App() {
     <>
       {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
       <div className="container my-3">
         <TextForm heading="Enter the text to analyze" mode={mode} />
         {/* <About/> */}
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -23,28 +23,28 @@ export default function TextForm(props) {
         // console.log("On change");
         setText(event.target.value)
     }
-    const handleCopy = ()=>{
-        
+    const handleCopy = () => {
+
         var text = document.getElementById("myBox");
         text.select();
         navigator.clipboard.writeText(text.value);
     }
-    const handleExtraSpaces = ()=>{
+    const handleExtraSpaces = () => {
         let newText = text.split(/[ ]+/);
         setText(newText.join(" "));
     }
 
 
 
-    const [text, setText] = useState('Enter the text here');
+    const [text, setText] = useState('');
     // text = "new text"; // Wrong way to change the state
     // setText("new text"); // Correct way to change the state
     return (
         <>
-            <div className="container"style={{color: props.mode === 'dark' ? 'white' : 'black'}}>
+            <div className="container" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
-                    <textarea className="form-control" value={text} onChange={handleOnChange}style={{backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode === 'dark' ? 'white' : 'black'}} id="myBox" rows="8"></textarea>
+                    <textarea className="form-control" value={text} placeholder="Enter the text here" onChange={handleOnChange} style={{ backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }} id="myBox" rows="8"></textarea>
                 </div>
                 <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
                 <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
@@ -52,12 +52,12 @@ export default function TextForm(props) {
                 <button className="btn btn-primary mx-2" onClick={handleCopy}> Copy Text</button>
                 <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}> Remove extra Spaces</button>
             </div>
-            <div className="container my-3"style={{color: props.mode === 'dark' ? 'white' : 'black'}}>
+            <div className="container my-3" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <h1>Your text Summary</h1>
                 <p>{text.split(" ").length} words and  {text.length} characters</p>
                 <p>{0.008 * text.split(" ").length} Minutes read</p>
                 <h2>Preview</h2>
-                <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
+                <p>{text.length > 0 ? text : "Enter something in the textbox above to preview it here"}</p>
             </div>
         </>
 
